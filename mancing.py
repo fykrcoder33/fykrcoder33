e59407570a5d34c0d75179f12231907810c950b3
fykrcoder33 <87960945+fykrcoder33@users.noreply.github.com>
2021-07-27 17:45:33 +0700

    Update mancing.py

--- mancing.py
+++ mancing.py
@@ -1,63 +1,54 @@
 #!/usr/bin/env python3
-import time, asyncio, sys
+import time, asyncio, sys, random
 
 
-from telethon import TelegramClient, events, utils
+from telethon import TelegramClient, events, utils, Button
 
 api_id = 2394927 
 api_hash = '4ab4c48f456d11a2aef03310c88c0b81'
-sesi_file = 'casino'
+sesi_file = 'mancing-fnz'
 
-Casino = 'All Sea'
+bot_id = 'KampungMaifamBot'
+Area = input('Area = ')
+Alat = input('Alat = ')
 
-    
 with TelegramClient(sesi_file, api_id, api_hash) as client:
-    client.loop.run_until_complete(client.send_message('KampungMaifamBot', Casino))
+        client.loop.run_until_complete(client.send_message(bot_id, Area))
+        @client.on(events.NewMessage(from_users=bot_id))
+        async def handler(event):
+            pesan = event.raw_text
 
-    @client.on(events.NewMessage(from_users='KampungMaifamBot'))
-    async def handler(event):
-        if 'Bagian kecil' in event.raw_text:
-            time.sleep(1)
-            await event.click(text='Tarik Jala')
-            return
-        
-        if 'Kamu berhasil menangkap' in event.raw_text:
-            time.sleep(1)
-            await event.respond('All Sea')
-            return
-        
-        if 'Kamu tidak memiliki cukup energi' in event.raw_text:
-            time.sleep(1)
-            await event.respond('/restore_max')
-            return
-        
-        if 'Restore 4000 energi' in event.raw_text:
-            time.sleep(1)
-            await event.click(text='Confirm')
-            return
-        
-        if 'Energi berhasil ' in event.raw_text:
-            time.sleep(1)
-            await event.respond('All Sea')
-            return
-        
-        if 'Tunggu minimal' in event.raw_text:
-            time.sleep(1)
-            await event.respond('/fishpoint_500')
-            return
-        
-        if 'Berhasil membeli' in event.raw_text:
-            time.sleep(1)
-            await event.respond('/umpanjala_1000')
-            return
-        
-        if 'Kamu membeli' in event.raw_text:
-            time.sleep(1)
-            await event.respond('All Sea')
-            return
-        
-    client.start() 
-    client.run_until_disconnected() 
-    print(time.asctime(), '-', 'berhenti')
-	
-	
+
+            if "Kamu berhasil" in pesan:
+                time.sleep(1)
+                await event.respond(Area)
+                print(pesan)
+                return
+                
+            elif 'Kamu tidak memiliki' in pesan:
+                time.sleep(2)
+                await event.respond('/restore')
+                print(time.asctime(), 'Isi Ulang Energi')
+                return
+                
+            elif "Kamu tidak sedang" in pesan:
+                time.sleep(1)
+                await event.respond(Area)
+                print('Lanjut Gan')
+                return
+                
+            elif 'Energi berhasil' in pesan:
+                time.sleep(2)
+                await event.respond(Area)
+                return
+            
+            else :
+                time.sleep(1)
+                await event.click(text=Alat)
+                print(time.asctime(), 'Lempar pancingan')
+                return
+            
+            
+client.start()
+client.run_until_disconnected()
+print(time.asctime(), '-', 'Berhenti')

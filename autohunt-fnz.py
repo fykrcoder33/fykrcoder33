9261da1632e91287e193ed3399248268fea0b144
fykrcoder33 <87960945+fykrcoder33@users.noreply.github.com>
2021-08-04 20:14:52 +0700

    Update autohunt-fnz.py

--- autohunt-fnz.py
+++ autohunt-fnz.py
@@ -6,11 +6,11 @@ from telethon import TelegramClient, events, utils, Button
 
 api_id = 2394927 
 api_hash = '4ab4c48f456d11a2aef03310c88c0b81'
-sesi_file = 'autohunt-fnz'
+sesi_file = 'autohunt-fnz1'
 
 bot_id = 'KampungMaifamXBot'
 Area = input('Area = ')
-Alat = input('Alat = ')
+Alat = 'Tangkap'
 
 with TelegramClient(sesi_file, api_id, api_hash) as client:
         client.loop.run_until_complete(client.send_message(bot_id, Area))
@@ -34,11 +34,16 @@ with TelegramClient(sesi_file, api_id, api_hash) as client:
                 time.sleep(2)
                 await event.respond(Area)
                 print(pesan)
+                return
+
+            elif "Kamu berburu" in pesan:
+                time.sleep(2)
+                await event.respond(Alat)
                 return    
                 
             elif 'Kamu tidak memiliki' in pesan:
                 time.sleep(2)
-                await event.respond('/restore')
+                await event.respond('/restore_max_confirm')
                 print(time.asctime(), 'Isi Ulang Energi')
                 return
                 
